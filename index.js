201e1fd368f8c792e3a1332ddbcdee32e7488383
kdosa <kdosanjh@csus.edu>
2022-06-12 23:52:30 -0700

    all form values can be stored now

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 
     //form can be filled and stored on local storage 
-
-
+    // run this command "localStorage.getItem('itemsJson')" on console
+    // to check the data submitted on form 
     add = document.getElementById("click");
+
     add.addEventListener("click", ()=>{
         emId = document.getElementById('id').value;
         fnm = document.getElementById('firstName').value;
         lnm = document.getElementById('lastName').value;
         ag = document.getElementById('age').value;
-        gndr = document.getElementById('gender').value;
-        hgt = document.getElementById('height1').value, document.getElementById('height2').value;
+        gndr = document.getElementById('gender');
+        gen = gndr.options[gndr.selectedIndex].text;
+        hgt = document.getElementById('height').value;
         wgt = document.getElementById('weight').value; 
         bdtmp = document.getElementById('bodyTemp').value;
         pulsrt = document.getElementById('pulseRate').value;
-        bldprssr = document.getElementById('bloodPrssr1').value, document.getElementById('bloodPrssr2').value;
+        bldprssr1 = document.getElementById('systolic');
+        systl = bldprssr1.options[bldprssr1.selectedIndex].text;
+        bldprssr2 = document.getElementById('diastolic');
+        diastl = bldprssr2.options[bldprssr2.selectedIndex].text;
         resprt = document.getElementById('respRate').value;
         avgexr = document.getElementById('avgExercise').value;
         vacbln = document.getElementById('vacBalnc').value;
         avgwrk = document.getElementById('avgWork').value;
 
+    
         if(localStorage.getItem('itemsJson')==null){
             itemJsonArray =[];
-            itemJsonArray.push([emId, fnm, lnm, ag, gndr, hgt, wgt, bdtmp, pulsrt, bldprssr, resprt, avgexr, vacbln, avgwrk]);
+            itemJsonArray.push([emId, fnm, lnm, ag, gen, hgt, wgt, bdtmp, pulsrt, systl, diastl, resprt, avgexr, vacbln, avgwrk]);
             localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
         }
         else{
             itemJsonArrayStr =localStorage.getItem('itemsJson');
             itemJsonArray = JSON.parse(itemJsonArrayStr);
-            itemJsonArray.push([emId, fnm, lnm, ag, gndr, hgt, wgt, bdtmp, pulsrt, bldprssr, resprt, avgexr, vacbln, avgwrk]);
+            itemJsonArray.push([emId, fnm, lnm, ag, gen, hgt, wgt, bdtmp, pulsrt, systl, diastl, resprt, avgexr, vacbln, avgwrk]);
             localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
         }
     })
