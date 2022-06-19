bebbd4345d00c07a7f74ba9210238997d577166e
josh_osuna <joshueosuna17@gmail.com>
2022-06-18 21:30:56 -0700

    Connected to Vendia
    This is connected to Vendia

--- add_employee.js
+++ add_employee.js
@@ -1,5 +1,194 @@
+import { createVendiaClient } from '@vendia/client';
 
-    //form can be filled and stored on local storage 
+const client = createVendiaClient({
+  apiUrl: `https://arimkqyeak.execute-api.us-west-2.amazonaws.com/graphql/`,
+  websocketUrl: `wss://1ckn28vxue.execute-api.us-west-2.amazonaws.com/graphql`,
+  apiKey: '6BNK5vfb4N3VEqqR1c2vQZsAUaToypXZMer2owLqEZ3u',
+});  
+const { entities } = client;
+
+
+
+async function async_list() {
+    console.log('Calling async_list function');
+    const employeeResponse = await entities.employee.list();
+    console.log(employeeResponse);
+}
+async_list();
+
+
+
+async function async_remove() {
+    console.log('Calling async_remove function');
+    const employeeResponse = await entities.employee.remove('018179af-8528-cb56-9dc3-95e659ae69b0');
+    console.log(employeeResponse);
+}
+//async_remove();
+
+
+
+export async function async_add() {
+    console.log('Calling async_remove function');
+    const employeeResponse = await entities.employee.add(
+        {
+        emp_id: 4,
+        firstName: "Tim",
+        lastName: "Turner",
+        age: 23,
+        genderIdentity: 'NonbinaryNonconforming',
+        height: 71,
+        weight: 195,
+        BodyTemperature: 99,
+        pulseRate: 85,
+        bloodPressureSystolic: 120,
+        bloodPressureDiastolic: 79,
+        respirationRate: 18,
+        exerciseHours: 0,
+        vacationBalance: 50,
+        workHours: 60
+        }
+    );
+    console.log(employeeResponse);
+}
+//async_add();
+
+//let add = document.getElementById("click");
+//add.addEventListener("click", async_add);
+
+
+
+
+/*
+add.addEventListener("click", () =>
+{
+            // Add an item with an ACL specifying that all nodes are restricted to read-only access w/out PII
+    console.log("made it here");
+    const response = await entities.employee.add
+    (
+        {
+            emp_id: document.getElementById('id').value,
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            age: document.getElementById('age').value,
+            genderIdentity: document.getElementById('gender').value,
+            genderSelector: genderIdentity.options[genderIdentity.selectedIndex].text,
+            height: document.getElementById('height').value,
+            weight: document.getElementById('weight').value,
+            bodyTemperature: document.getElementById('bodyTemp').value,
+            pulseRate: document.getElementById('pulseRate').value,
+            bloodPressureSystolic: document.getElementById('systolic').value,
+            bloodPressureDiastolic: document.getElementById('diastolic').value,
+            respirationRate: -document.getElementById('respRate').value,
+            exerciseHours: document.getElementById('avgExercise').value,
+            vacationBalance: -document.getElementById('vacBalnc').value,
+            workHours: document.getElementById('avgWork').value, 
+        },
+        {
+            aclInput: 
+            {
+                acl: 
+                [
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "emp_id",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "age",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "genderIdentity",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "height",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "weight",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "bodyTemperature",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "pulseRate",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "bloodPressureSystolic",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "bloodPressureDiastolic",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "respirationRate",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "exerciseHours",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "vacationBalance",
+                        operations: [READ]
+                    },
+                    {
+                        principal:{
+                            nodes:["*"]
+                        },
+                        path: "workHours",
+                        operations: [READ]
+                    },
+                ],
+            },
+        },
+    );
+});
+*/
+
+
+/*
+   //form can be filled and stored on local storage 
     // run this command "localStorage.getItem('itemsJson')" on console
     // to check the data submitted on form 
     
@@ -36,3 +225,4 @@
             localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
         }
     })
+ */
