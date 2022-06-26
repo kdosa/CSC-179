6a6041a8660969b1de7c25e8348713538785076b
Mehton <92951420+Mehton@users.noreply.github.com>
2022-06-26 13:38:20 -0700

    Create drop_down.js

---
+++ drop_down.js
@@ -0,0 +1,59 @@
+function show_table1()
+{
+    document.getElementById('d1').removeAttribute('hidden');
+    document.getElementById('d2').setAttribute('hidden','hidden');
+    document.getElementById('d3').setAttribute('hidden','hidden');
+    document.getElementById('d4').setAttribute('hidden','hidden');
+    document.getElementById('d5').setAttribute('hidden','hidden');
+    document.getElementById('d6').setAttribute('hidden','hidden');
+}
+
+function show_table2()
+{
+    document.getElementById('d2').removeAttribute('hidden');
+    document.getElementById('d1').setAttribute('hidden','hidden');
+    document.getElementById('d3').setAttribute('hidden','hidden');
+    document.getElementById('d4').setAttribute('hidden','hidden');
+    document.getElementById('d5').setAttribute('hidden','hidden');
+    document.getElementById('d6').setAttribute('hidden','hidden');
+}
+
+function show_table3()
+{
+    document.getElementById('d3').removeAttribute('hidden');
+    document.getElementById('d1').setAttribute('hidden','hidden');
+    document.getElementById('d2').setAttribute('hidden','hidden');
+    document.getElementById('d4').setAttribute('hidden','hidden');
+    document.getElementById('d5').setAttribute('hidden','hidden');
+    document.getElementById('d6').setAttribute('hidden','hidden');
+}
+
+function show_table4()
+{
+    document.getElementById('d4').removeAttribute('hidden');
+    document.getElementById('d1').setAttribute('hidden','hidden');
+    document.getElementById('d2').setAttribute('hidden','hidden');
+    document.getElementById('d3').setAttribute('hidden','hidden');
+    document.getElementById('d5').setAttribute('hidden','hidden');
+    document.getElementById('d6').setAttribute('hidden','hidden');
+}
+
+function show_table5()
+{
+    document.getElementById('d5').removeAttribute('hidden');
+    document.getElementById('d1').setAttribute('hidden','hidden');
+    document.getElementById('d2').setAttribute('hidden','hidden');
+    document.getElementById('d3').setAttribute('hidden','hidden');
+    document.getElementById('d4').setAttribute('hidden','hidden');
+    document.getElementById('d6').setAttribute('hidden','hidden');
+}
+
+function show_table6()
+{
+    document.getElementById('d6').removeAttribute('hidden');
+    document.getElementById('d1').setAttribute('hidden','hidden');
+    document.getElementById('d2').setAttribute('hidden','hidden');
+    document.getElementById('d3').setAttribute('hidden','hidden');
+    document.getElementById('d4').setAttribute('hidden','hidden');
+    document.getElementById('d5').setAttribute('hidden','hidden');
+}
